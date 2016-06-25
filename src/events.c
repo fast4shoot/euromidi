07c42aa17a5f6ed09b9d090e0e20b36ffbc73157
Zdeněk Biberle <xbiber00@stud.fit.vutbr.cz>
2016-06-26 00:58:05 +0200

    Added a few compiler switches

--- src/events.c
+++ src/events.c
@@ -1,21 +1,23 @@
 #include "events.h"
 
 static event_t event_queue[EVENT_QUEUE_SIZE];
-static uint8_t event_queue_begin = 0;
-static uint8_t event_queue_end = 0;
+static volatile uint8_t event_queue_begin = 0;
+static volatile uint8_t event_queue_end = 0;
 
 void event_push(event_t event) {
 	// this may overflow the queue, we just have to hope it doesn't
-	event_queue[event_queue_end] = event;
-	event_queue_end = (event_queue_end + 1) % EVENT_QUEUE_SIZE;
+	uint8_t end = event_queue_end;
+	event_queue[end] = event;
+	event_queue_end = (end + 1) % EVENT_QUEUE_SIZE;
 }
 
 bool event_peek(event_t* event) {
-	if (event_queue_begin == event_queue_end) {
+	uint8_t begin = event_queue_begin;
+	if (begin == event_queue_end) {
 		return false;
 	} else {
-		*event = event_queue[event_queue_begin];
-		event_queue_begin = (event_queue_begin + 1) % EVENT_QUEUE_SIZE;
+		*event = event_queue[begin];
+		event_queue_begin = (begin + 1) % EVENT_QUEUE_SIZE;
 		return true;
 	}
 }
